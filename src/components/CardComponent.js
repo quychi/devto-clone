f5b851bfc61c1808718c70758316a2ec961594ac
quychi <nguyenquychi96@gmail.com>
2021-07-30 11:13:54 +0700

    right side bar

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -1,7 +1,32 @@
 import React from "react";
 
-const CardComponent = () => {
-  return <div>CardComponent</div>;
+const CardComponent = (props) => {
+  const { tag, articles } = props;
+  return (
+    <div className="card">
+      <header>
+        <h3>{tag}</h3>
+        {tag === "Listings" && (
+          <a href="/#">
+            <small>see all</small>
+            {/* small tag for small text */}
+          </a>
+        )}
+      </header>
+      <ul>
+        {articles.map((a) => {
+          return (
+            <li key={a.id}>
+              <a href="/#">{a.mainTitle}</a> <br />
+              <small>{a.subText}</small>
+              {a.newarticle && <span>new</span>}
+              {/*  use span + css instead of button          */}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default CardComponent;
