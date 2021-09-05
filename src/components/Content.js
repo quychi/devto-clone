92f3adabc01b02b78214f11bf0ac132972851302
quychi <nguyenquychi96@gmail.com>
2021-09-05 23:17:46 +0700

    Article Section + time distance dayjs

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ArticleComponent from "./ArticleComponent";
 
 const Content = () => {
-  return <div>Content</div>;
+  const [articles, setArticles] = useState(null);
+
+  useEffect(() => {
+    setTimeout(async () => {
+      const res = await fetch("https://dev.to/api/articles");
+      const data = await res.json();
+
+      setArticles(data);
+    }, 2000);
+  }, []);
+
+  return (
+    <main className="main-content">
+      <header>
+        <h1>Posts</h1>
+        <nav>
+          <a href="#">Feed</a>
+          <a href="#">Week</a>
+          <a href="#">Month</a>
+          <a href="#">Infinity</a>
+          <a href="#">Latest</a>
+        </nav>
+
+        <select id="dropdown-select" className="dropdown">
+          <option value="Feed" defaultValue>
+            Feed
+          </option>
+          <option value="Week">Week</option>
+          <option value="Month">Month</option>
+          <option value="Infinity">Infinity</option>
+          <option value="Latest">Latest</option>
+        </select>
+      </header>
+
+      {articles?.map((article, id) => {
+        return <ArticleComponent key={id} data={article} />;
+      })}
+    </main>
+  );
 };
 
 export default Content;
