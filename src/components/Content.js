fbaab51e6ad3ce86dde1c97881595c477460c9ce
quychi <nguyenquychi96@gmail.com>
2021-09-06 15:24:37 +0700

    Infinite Loading

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,6 +5,41 @@ const Content = () => {
   const [articles, setArticles] = useState(null);
 
   useEffect(() => {
+    const fetchAgain = () => {
+      if (articles != null) {
+        fetch("https://dev.to/api/articles") //need to change the api (calling the api results in the next post)
+          .then((res) => res.json())
+          .then((result) => setArticles([...articles, ...result]));
+      }
+    };
+
+    const handleScroll = () => {
+      const html = document.documentElement;
+      const body = document.body;
+      const windowheight =
+        "innerHeight" in window ? window.innerHeight : html.offsetHeight;
+
+      const docHeight = Math.max(
+        body.scrollHeight,
+        body.offsetHeight,
+        html.clientHeight,
+        html.scrollHeight,
+        html.offsetHeight
+      );
+
+      const windowBottom = windowheight + window.pageYOffset;
+      if (windowBottom >= docHeight) {
+        console.log("we reached the bottom");
+        fetchAgain();
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [articles]);
+
+  useEffect(() => {
     setTimeout(async () => {
       const res = await fetch("https://dev.to/api/articles");
       const data = await res.json();
